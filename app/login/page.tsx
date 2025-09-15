74bbbc09704583b6a2d8d937fa3bc4c7cd03f4eb
henches <henches@gmail.com>
2025-09-15 17:29:47 +0200

    Suppresion ident/authent et gestion des epaces et des "

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,47 +4,48 @@ import { useRouter } from "next/navigation";
 import { Form, FormProps, Input } from 'antd';
 import { checkLoginRequest, LoginRequest } from "../actions/users.action";
 
+// Ident/authent non utilisépour l'instant
 
-export default function LoginPage() {
-  const router = useRouter();
+// export default function LoginPage() {
+//   const router = useRouter();
 
-  type LoginInfo = {
-    userName?: string
-    password?: string
-  };
+//   type LoginInfo = {
+//     userName?: string
+//     password?: string
+//   };
 
-  const onFinish: FormProps<LoginInfo>['onFinish'] = async (values: LoginInfo) => {
-    console.log("onFinish = values", values)
-    const loginRequest: LoginRequest = {
-      userName: values.userName ?? '',
-      password: values.password ?? '',
-    }
-    const result = await checkLoginRequest(loginRequest);
-    console.log('result = ', result)
-    router.push("/"); 
-    router.refresh();
-  };
+//   const onFinish: FormProps<LoginInfo>['onFinish'] = async (values: LoginInfo) => {
+//     console.log("onFinish = values", values)
+//     const loginRequest: LoginRequest = {
+//       userName: values.userName ?? '',
+//       password: values.password ?? '',
+//     }
+//     const result = await checkLoginRequest(loginRequest);
+//     console.log('result = ', result)
+//     router.push("/");
+//     router.refresh();
+//   };
 
 
-  return (
-    <main style={{ minHeight: "100dvh", display: "grid", placeItems: "center" }}>
-      <Form onFinish={onFinish} style={{ display: "grid", gap: 12, minWidth: 280 }}>
-        <h1>Wi's noch komm't</h1>
-        <Form.Item<LoginInfo> label="Prénom" name="userName" rules={[{ required: true, message: 'Merci de saisir le prénom' }]}  >
-          <Input
-            placeholder="prénom (sans majuscule)"
-            required
-          />
-        </Form.Item>
-        <Form.Item<LoginInfo> label="Mot de passe" name="password" rules={[{ required: true, message: 'Merci de saisir le mot de passe' }]}  >
-          <Input
-            type="password"
-            placeholder="mot de passe"
-            required
-          />
-        </Form.Item>
-        <button type="submit">Se connecter</button>
-      </Form>
-    </main>
-  );
-}
+//   return (
+//     <main style={{ minHeight: "100dvh", display: "grid", placeItems: "center" }}>
+//       <Form onFinish={onFinish} style={{ display: "grid", gap: 12, minWidth: 280 }}>
+//         <h1>Wi's noch komm't</h1>
+//         <Form.Item<LoginInfo> label="Prénom" name="userName" rules={[{ required: true, message: 'Merci de saisir le prénom' }]}  >
+//           <Input
+//             placeholder="prénom (sans majuscule)"
+//             required
+//           />
+//         </Form.Item>
+//         <Form.Item<LoginInfo> label="Mot de passe" name="password" rules={[{ required: true, message: 'Merci de saisir le mot de passe' }]}  >
+//           <Input
+//             type="password"
+//             placeholder="mot de passe"
+//             required
+//           />
+//         </Form.Item>
+//         <button type="submit">Se connecter</button>
+//       </Form>
+//     </main>
+//   );
+// }
